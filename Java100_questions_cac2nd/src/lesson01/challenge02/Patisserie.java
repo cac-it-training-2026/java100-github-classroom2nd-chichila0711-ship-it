ba88e784b9fe6893d35891cd46a6f51445d88aba
tsuzurao <tsuzurao@C010423066.zipangu.cac.co.jp>
2026-04-15 10:39:56 +0900

    lesson01challenge02

--- Java100_questions_cac2nd/src/lesson01/challenge02/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge02/Patisserie.java
@@ -1,5 +1,9 @@
 /**
  * 第1章 マカロン屋さんのお仕事
+
+
+
+
  *
  * 問題2 商品の展示
  *
@@ -19,5 +23,16 @@
 package lesson01.challenge02;
 
 public class Patisserie {
+	public static void main(String[] args) {
+		System.out.println("大変お待たせしました。");
+		System.out.println("【ポエール・ネルメ】");
+		System.out.println("ただいまより開店です！！");
+
+		System.out.println("\n本日のおすすめ商品です。\n");
+		System.out.println("シトロン　    \\250");
+		System.out.println("ショコラ    　\\280");
+		System.out.println("ピスターシュ  \\320");
+
+	}
 
 }
